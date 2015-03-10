65a964077cf87e0568ab93024e930736eb722149
Lucas <lucasdnd@gmail.com>
2015-03-10 02:08:30 -0300

    Playing random noise

--- noise.py
+++ noise.py
@@ -2,8 +2,9 @@ import os
 import time
 import random
 import string
+import math
 
-size = 5000
+buffer_size = 8192 # min rquired to play something
 
 def write_random_data():
   s = ""
@@ -13,14 +14,17 @@ def write_random_data():
 
 def write_bytes(b):
   s = ""
-  print("playing: " + str(bin(b)[2:]).rjust(8, '0') + " (" + str(b) + ")")
-  for i in range(0, size):
+  print("playing: " + b)
+  for i in range(0, buffer_size // math.floor(math.sqrt(len(b)))):
     s = s + str(b)
   return s
 
-for i in range(0, 255):
-  s = write_bytes(i)
+for i in range(0, 4):
+  s = write_bytes(random.sample(string.printable, 1)[0] + "0")
   f = open("data.txt", "w")
   f.write(s)
+  f.write("0")
+  #start_time = time.time()
   os.system("cat data.txt | padsp tee /dev/audio > /dev/null")
+  #print("played during: " + str(time.time() - start_time))
   f.close()
